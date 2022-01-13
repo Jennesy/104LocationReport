a4f10a16602c3089a196ef94faa640833f0ec90f
Jennesy <jennesy.dai@gmail.com>
2022-01-13 15:45:23 +0800

    feature: change time display / if at home, hide related inputs

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
+renewTime()
+function renewTime() {
+  const currentTime = new Date();
+  document.querySelector("#current-hour").value = currentTime.getHours();
+  document.querySelector("#current-minute").value = currentTime.getMinutes();
+}
 
-const atHome = document.querySelector("#at-home");
 const showInputBox = (box, show) => {
   if (show) {
     box.style.transform = "scale(1, 1)"
     box.disabled = false;
     box.focus();
+    box.nextElementSibling.style.visibility = "visible";
   } else {
     box.style.transform = "scale(1, 0)";
     box.disabled = true;
+    box.nextElementSibling.style.visibility = "hidden";
+  }
+}
+const showReturnTime = (show) => {
+  const returnTime = document.querySelector("#return-time");
+  const transportation = document.querySelector("#transportation");
+  if (show) {
+    returnTime.style.display = "block";
+    transportation.style.display = "block";
+    document.querySelector("#walk").required = true;
+  } else {
+    returnTime.style.display = "none";
+    transportation.style.display = "none";
+    document.querySelector("#walk").required = false;
   }
 }
 
+const atHome = document.querySelector("#at-home");
 const locate = atHome.parentNode.nextElementSibling;
 atHome.addEventListener("change", () => {
   showInputBox(locate, !atHome.checked);
+  showReturnTime(!atHome.checked);
 });
 
 const inputTextToggle = document.querySelectorAll(".input-text-toggle");
